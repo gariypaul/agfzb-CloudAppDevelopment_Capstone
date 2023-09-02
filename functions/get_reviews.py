83cd327e07ff8a59a2d3a0a17f63085544c72b8b
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2023-09-01 21:03:21 -0500

    U func

--- functions/get_reviews.py
+++ functions/get_reviews.py
@@ -45,7 +45,10 @@ def post_review():
 
     # Validate that the required fields are present in the review data
     required_fields = ['id', 'name', 'dealership', 'review', 'purchase', 'purchase_date', 'car_make', 'car_model', 'car_year']
-    print(f"{review_data['name']}")
+    try:
+        print(f"{review_data['name']}")
+    except Exception as e: 
+        print(str(e))
     for field in required_fields:
         if field not in review_data:
             print(f"Missing required field: {field}")
